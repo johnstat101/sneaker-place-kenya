f9b5ec5bae8958ac919b625e01115609d144283a
Rea <rmrea@kenya-airways.com>
2021-10-31 22:39:28 +0300

    add a constructor for prices

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,24 @@
 //Constructor for user inputs
-function userInputs(name, prices){
+function userInputs(name, sizePrize,crustPrize,toppingsPrize){
+    //convert NaN inputs to 0
+    crustPrize=crustPrize? crustPrize : 0;
+    toppingsPrize=toppingsPrize? toppingsPrize : 0;
+
     this.name = name;
-    this.prices = prices;
+    this.sizePrize = sizePrize;
+    this.crustPrize = crustPrize;
+    this.toppingsPrize = toppingsPrize;
 }
 
+//Define a prototype function to get totals
 userInputs.prototype.userTotal = function(){
-    return this.size + this.toppings + this.crust
+    return this.sizePrize + this.toppingsPrize + this.crustPrize
 }
 
-// define prices
-function sizePrize(){
-    this.small = 400;
-    this.medium = 500;
-    this.large = 600;
-}
-function crustPrize(){
-    this.crispy = 50;
-    this.stuffed = 80;
-    this.glutenFree = 100;
-}
-function toppingsPrize(){
-    this.pepperoni = 80;
-    this.mushrooms = 60;
-    this.onions = 50;
-    this.extraCheese = 45;
-}
-
-
+// define prices objects
+var sizePrize = {small: 400, medium: 500, large: 600}
+var crustPrize = {crispy: 50, stuffed: 80, glutenFree: 100}
+var toppingsPrize = {pepperoni: 80, mushrooms: 60, onions: 50, extraCheese: 45}
 
 // user logic
 $(document).ready(function(){
@@ -38,8 +30,8 @@ $(document).ready(function(){
         var crust = $("#crust").val();
         var toppings = $("#toppings").val();
 
-        var newUser = new userInputs(name, [sizePrize.size, crustPrize.crust, toppingsPrize.toppings])
-        console.log(newUser);
+        var newUser = new userInputs(name, sizePrize[size], crustPrize[crust], toppingsPrize[toppings]);
+        console.log(newUser.userTotal());
 
     })
 
