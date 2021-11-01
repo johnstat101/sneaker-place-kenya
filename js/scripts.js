e859daf22d6bf21c6698acb33405e8a3830d7bb8
Rea <rmrea@kenya-airways.com>
2021-11-01 15:44:58 +0300

    append user totals to html page

--- js/scripts.js
+++ js/scripts.js
@@ -25,18 +25,19 @@ var toppingsPrize = {pepperoni: 80, mushrooms: 60, onions: 50, extraCheese: 45}
 $(document).ready(function(){
     $("#btnAdd").click(function(event){
         event.preventDefault();
-//user input
+    //user input
         var name = $("#userName").val();
         var size = $("#size").val();
         var crust = $("#crust").val();
         var toppings = $("#toppings").val();
 
+        // create construct for user inputs
         var newUser = new userInputs(name, sizePrize[size], crustPrize[crust], toppingsPrize[toppings]);
         
         //Append row cost to user choice
         var total = newUser.userTotal();
         $("#orderSummary").append("<p id='order'>Pizza size: "+size+" Crust: "+crust+ " Toppings: "+toppings+" @KES. "+"<span id = 'rowTotal'>"+total+"</span>"+"</p>");
-        
+
         //include grand total
         var grantTotal = 0;
         $("p #rowTotal").each(function(){
@@ -45,11 +46,3 @@ $(document).ready(function(){
         $("#totals").text("TOTAL: "+grantTotal);
     })
 })
-
-// $(document).ready(function(){
-// var grantTotal = 0;
-// $("#rowTotal").each(function(){
-//     grantTotal += +$(this).text()||0;
-// });
-// $("#totals").text("TOTAL: "+grantTotal);
-// })
